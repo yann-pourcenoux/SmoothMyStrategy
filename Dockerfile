0b98ce504ad9f52b65ba8a426a438287cb14165c
Yann POURCENOUX <yann.pourcenoux@gmail.com>
2023-09-19 16:11:32 +0000

    35: download data script

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,9 @@ RUN apt install -y git
 # Install the package
 COPY setup.py .
 RUN pip install -e .
+
+# Add .local/bin to PATH
+RUN export PATH="$PATH:/home/vscode/.local/bin"
+
+# Add a non root user
+RUN adduser vscod
--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-from setuptools import setup  # type: ignore
+from setuptools import find_packages, setup
 
 STYLE_PKGS = [
     "black~=23.7.0",
     "docformatter~=1.7.5",
     "isort~=5.12.0",
-    "mypy~=1.4.1",
     "pre-commit~=3.3.3",
     "prettier~=0.0.7",
     "ruff~=0.0.280",
+]
+UTILS_PKGS = [
+    "GitPython~=3.1.35",
+    "jupyter~=1.0.0",
+]
+DS_PKGS = [
+    "pandas~=2.1.0",
+    "yfinance~=0.2.28",
+    "statsmodels~=0.14.0",
+]
+ML_PKGS = [
+    "xgboost~=2.0.0",
+    "scikit-learn~=1.3.0",
+]
+VIZ_PKGS = [
+    "seaborn~=0.12.2",
 ]
 
 TEST_PKGS = ["pytest~=7.4.0"]
@@ -20,5 +35,7 @@ setup(
     author="Yann POURCENOUX",
     author_email="yann.pourcenoux@gmail.com",
     url="https://gitlab.com/yannpourcenoux/Finance",
-    install_requires=STYLE_PKGS + TEST_PKGS,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=STYLE_PKGS + TEST_PKGS + UTILS_PKGS + DS_PKGS + ML_PKGS + VIZ_PKGS,
 )
