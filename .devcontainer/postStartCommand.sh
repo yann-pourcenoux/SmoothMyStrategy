36b164e6c9137ec056d3a2080132527dd37db73f
Yann POURCENOUX <yann.pourcenoux@gmail.com>
2024-02-02 15:17:44 +0000

    Clean setup

---
+++ .devcontainer/postStartCommand.sh
@@ -0,0 +1,4 @@
+# Add .local/bin to PATHm needed to find the packages
+export PATH=$PATH:/home/vscode/.local/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vscode/.mujoco/mujoco210/bin
+# wandb login 52084ad3dd36b2a1f4181e861ebfdb83e2376c72
