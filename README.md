a33ac490ca07cd523f8da857f4c0185f2f276d71
Yann POURCENOUX <yann.pourcenoux@gmail.com>
2024-03-23 23:18:25 +0000

    Add evaluation report and metrics

--- README.md
+++ README.md
@@ -4,3 +4,34 @@
 
 This project aims to create agents to manage investment portfolios.
 This will be achieved by using Deep Learning and Reinforcement Learning using Pytorch.
+
+## Setup
+
+You can install the package by doing the following command:
+
+```shell
+pip install -e .
+```
+
+## Data
+
+To download the data, run:
+
+```shell
+python src/data/download.py
+```
+
+## Train a model
+
+To train a model, run:
+
+```shell
+python src/run_training.py --config-name=xxx
+```
+
+### Artificial data
+
+It is just a sigmoid that is generated in the downloading script
+
+You can run a training on it using the `debug` config, i.e using the `--config-name=debug`
+argument.
--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from setuptools import find_packages, setup
 
 DS_PKGS = [
-    "pandas~=2.1.0",
-    "yfinance~=0.2.28",
-    "statsmodels~=0.14.0",
+    "pandas~=2.2.1",
+    "yfinance~=0.2.37",
+    "statsmodels~=0.14.1",
 ]
 FIN_PKG = [
     "stockstats~=0.6.2",
-    "quantstats~=0.0.62",
+    "QuantStats",  # The version comes from the URL in dependency_links
 ]
 RL_PKGS = [
-    "tensordict~=0.3.0",
-    "torchrl~=0.3.0",
+    "tensordict~=0.3.1",
+    "torchrl~=0.3.1",
 ]
 STYLE_PKGS = [
     "black~=23.7.0",
@@ -24,20 +24,20 @@ STYLE_PKGS = [
     "ruff~=0.0.280",
 ]
 TEST_PKGS = [
-    "pytest~=7.4.0",
+    "pytest~=8.1.1",
 ]
 UTILS_PKGS = [
-    "GitPython~=3.1.35",
+    "GitPython~=3.1.42",
     "icecream~=2.1.3",
     "jupyter~=1.0.0",
     "loguru~=0.7.2",
-    "pydantic~=2.5.2",
+    "pydantic~=2.6.4",
     "hydra-core~=1.3.2",
 ]
 VIZ_PKGS = [
-    "plotly~=5.19.0",
-    "seaborn~=0.12.2",
-    "wandb==0.15.0",
+    "plotly~=5.20.0",
+    "seaborn~=0.13.2",
+    "wandb~=0.16.4",
 ]
 PKGS = DS_PKGS + FIN_PKG + RL_PKGS + STYLE_PKGS + TEST_PKGS + UTILS_PKGS + VIZ_PKGS
 
@@ -48,6 +48,7 @@ setup(
     author="Yann POURCENOUX",
     author_email="yann.pourcenoux@gmail.com",
     url="https://gitlab.com/yannpourcenoux/Finance",
+    dependency_links=["https://github.com/anchorblock/quantstats"],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=PKGS,
