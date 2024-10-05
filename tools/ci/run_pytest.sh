b9a90682d32591c328ce1521149f5a1ac152a2f1
Yann POURCENOUX <yann.pourcenoux@gmail.com>
2024-10-05 15:41:53 +0200

    update gitlab file

--- tools/ci/run_pytest.sh
+++ tools/ci/run_pytest.sh
@@ -8,4 +8,5 @@ echo $GIT_PYTHON_GIT_EXECUTABLE
 mkdir -p data
 
 python src/data/download.py --files src/data/config/test_data.json
-pytest --cov=src --cov-report=xml src
+pytest --cov --cov-report term --cov-report xml:coverage.xml src
+
